2f4a0f2bcd20a2f5d40f8d22cd9870e4a7857643
pantpad <138708449+pantpad@users.noreply.github.com>
2023-07-29 14:51:38 +0200

    Add comments to script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log('ready');
+
+//create the visual representation of the calculator
+//make the numbers clicked appear on the calculator
+//when operator is clicked 
+    //save what is inside the display -> firstValue || CALCULATE firstValue + operator + secondValue and save it inside the firstValue
+    //save the operator clicked
+//make the numbers clicked appear on the calculator
+//another operator is clicked
+    //equality operator with firstValue and secondValue != null? -> calculate firstValue + operator + secondValue;
+    //
+
