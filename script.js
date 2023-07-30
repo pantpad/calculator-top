7edf421a07d418104edb084014ad2efb647409a0
pantpad <138708449+pantpad@users.noreply.github.com>
2023-07-30 15:52:35 +0200

    Create Computational Logic for basic operations

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 console.log('ready');
-
+const DECIMAL_PRECISION = 2;
 //create the visual representation of the calculator
 //make the numbers clicked appear on the calculator
 //when operator is clicked 
-    //save what is inside the display -> firstValue || CALCULATE firstValue + operator + secondValue and save it inside the firstValue
+    //save what is inside the display -> firstNumber || CALCULATE firstNumber + operator + secondNumber and save it inside the firstNumber
     //save the operator clicked
 //make the numbers clicked appear on the calculator
 //another operator is clicked
-    //equality operator with firstValue and secondValue != null? -> calculate firstValue + operator + secondValue;
+    //equality operator with firstNumber and secondNumber != null? -> calculate firstNumber + operator + secondNumber;
     
 const calculatorBox = document.querySelector('.calculator-box');
 const display = document.querySelector('.display');
@@ -21,3 +21,46 @@ function reduceDisplayText(){
     displayContent.style.fontSize = parseFloat(window.getComputedStyle(displayContent).fontSize) - 10 + "px";
 }
 
+function add(firstNumber,secondNumber){
+    return firstNumber + secondNumber;
+}
+
+function subtract(firstNumber,secondNumber){
+    return firstNumber - secondNumber;
+}
+function multiply(firstNumber,secondNumber){
+    
+    if((firstNumber * secondNumber)%1 !== 0){
+        return parseFloat((firstNumber * secondNumber).toFixed(DECIMAL_PRECISION));
+    }else{
+        return firstNumber * secondNumber;
+    }
+}
+function divide(firstNumber,secondNumber){
+    if((firstNumber / secondNumber)%1 !== 0){
+        return parseFloat((firstNumber / secondNumber).toFixed(DECIMAL_PRECISION));
+    }else{
+        return firstNumber / secondNumber;
+    }
+}
+/**
+ * takes an operator and DECIMAL_PRECISION numbers and then calls one of the above functions on the numbers.
+ */
+function operate(operator,firstNumber,secondNumber){
+    switch(operator){
+        case '+':
+            displayContent.textContent = add(firstNumber,secondNumber);
+            break;
+        case '-':
+            displayContent.textContent = subtract(firstNumber,secondNumber);
+            break;
+        case '*':
+            displayContent.textContent = multiply(firstNumber,secondNumber);
+            break;
+        case '/':
+            displayContent.textContent = divide(firstNumber,secondNumber);
+            break;
+    }
+}
+
+displayContent.textContent = "test";
